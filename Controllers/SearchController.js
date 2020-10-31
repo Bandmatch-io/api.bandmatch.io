3d43132328765da7fd4f5c6fdc139968202209fd
Tom Croasdale <tecroasdale@gmail.com>
2020-10-31 13:54:14 +0000

    Fixed searchendpoint

--- Controllers/SearchController.js
+++ Controllers/SearchController.js
@@ -14,43 +14,50 @@ module.exports.search = function (req, res, next) {
     return next()
   }
 
-  // Match search types
-  let types = []
-  if (req.user.searchType === 'Join') {
-    types = ['Recruit']
-  } else if (req.user.searchType === 'Form') {
-    types = ['Form', 'Either']
-  } else if (req.user.searchType === 'Either') {
-    types = ['Form', 'Recruit']
-  } else if (req.user.searchType === 'Recruit') {
-    types = ['Join', 'Either']
-  }
-
   User.findById(req.user._id, (err, currUser) => {
-    User.find({
-      searchLocation: {
-        $near: {
-          $maxDistance: currUser.searchRadius * 1000, // searchRadius to km
-          $geometry: { type: 'Point', coordinates: currUser.searchLocation.coordinates }
-        }
-      },
-      genres: { $in: currUser.genres },
-      instruments: { $in: currUser.instruments },
-      _id: { $ne: currUser._id },
-      searchType: { $in: types },
-      active: true
-    })
-      .select('_id displayName searchType fullSearchType genres instruments description')
-      .exec((err, users) => {
-        if (err) {
-          res.json({ success: false })
-        } else {
-          res.json({ success: true, matches: users })
-  
-          if (!currUser.admin) { // Don't incremement stat is admin
-            StatController.incrementStat('searches')
+    if (err) {
+      next(err)
+    } else {
+      if (!currUser) {
+        return res.status(401).json({ success: false, error: { login: { absent: true } } })
+      }
+      // Match search types
+      let types = []
+      if (currUser.searchType === 'Join') {
+        types = ['Recruit']
+      } else if (currUser.searchType === 'Form') {
+        types = ['Form', 'Either']
+      } else if (currUser.searchType === 'Either') {
+        types = ['Form', 'Recruit']
+      } else if (currUser.searchType === 'Recruit') {
+        types = ['Join', 'Either']
+      }
+
+      User.find({
+        searchLocation: {
+          $near: {
+            $maxDistance: currUser.searchRadius * 1000, // searchRadius to km
+            $geometry: { type: 'Point', coordinates: currUser.searchLocation.coordinates }
           }
-        }
+        },
+        genres: { $in: currUser.genres },
+        instruments: { $in: currUser.instruments },
+        _id: { $ne: currUser._id },
+        searchType: { $in: types },
+        active: true
       })
+        .select('_id displayName searchType fullSearchType genres instruments description')
+        .exec((err, users) => {
+          if (err) {
+            next(err)
+          } else {
+            res.json({ success: true, matches: users })
+    
+            if (!currUser.admin) { // Don't incremement stat is admin
+              StatController.incrementStat('searches')
+            }
+          }
+        })
+    }
   })
 }
